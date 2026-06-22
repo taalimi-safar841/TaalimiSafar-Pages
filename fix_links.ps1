a82234e40315eace459d5da86350ffdd86f43d0a
ZEESHAN RASHID <rashidzeeshan9702@gmail.com>
2026-06-23 01:49:18 +0530

    chore: remove temporary fix_links script

--- fix_links.ps1
+++
@@ -1,7 +0,0 @@
-$file = "C:\Users\athan\Desktop\Taalimi_Safar\taalimi-safar-pages\account-deletion.html"
-$content = Get-Content $file -Raw -Encoding UTF8
-$content = $content.Replace("const BASE = '';", "const BASE = 'https://taalimisafar-ko64k.ondigitalocean.app';")
-$content = $content.Replace("/account-deletion/", "./account-deletion.html")
-$content = $content.Replace("/privacy-policy/", "./privacy-policy.html")
-Set-Content $file $content -Encoding UTF8
-Write-Host "Done!"
